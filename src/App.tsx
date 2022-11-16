3a5488c29019f27faf32ef857f947294f1a8f0ca
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2022-11-16 13:37:43 +0400

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,15 @@ import {Todolist} from './Todolist';
 
 function App() {
 
-    const tasks1 = [
+    const task = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false }
     ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1} />
-            <Todolist title="How to learn" tasks={tasks2} />
+            <Todolist title="What to learn" tasks={task} />
         </div>
     );
 }
